57cf975919b04a90060efd0d9b21c98a967ce18e
Numwali <n.umwali@alustudent.com>
2024-10-05 09:52:42 +0200

    Deploy

--- Documents/alu-AirBnB_clone_v2/alu-AirBnB_clone_v2-master/2-do_deploy_web_static.py
+++ Documents/alu-AirBnB_clone_v2/alu-AirBnB_clone_v2-master/2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['100.27.187.5', '54.167.59.7']
+env.hosts = ['54.227.34.157', '52.23.160.214']
 
 
 def do_deploy(archive_path):
